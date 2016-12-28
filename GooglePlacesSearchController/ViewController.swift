78831672c943d5fb3e68134631e226d228b7d778
bhutenthinlay <bhutenthinlay@gmail.com>
2016-12-28 15:17:09 +0530

    Initial Commit

---
+++ GooglePlacesSearchController/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  GooglePlacesSearchController
+//
+//  Created by Dmitry Shmidt on 6/28/15.
+//  Copyright (c) 2015 Dmitry Shmidt. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+import GooglePlacesSearchController
+
+class ViewController: UIViewController {
+    let GoogleMapsAPIServerKey = "AIzaSyDIjJPQQ4yqsPSg3VxSOMo8ZRESkeXKCwQY"
+
+    @IBAction func searchAddress(_ sender: UIBarButtonItem) {
+        
+        let controller = GooglePlacesSearchController(
+            apiKey: GoogleMapsAPIServerKey,
+            placeType: PlaceType.address
+        )
+        
+//        Or if you want to use autocompletion for specific coordinate and radius (in meters)        
+//        let coord = CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423)
+//        let controller = GooglePlacesSearchController(
+//            apiKey: GoogleMapsAPIServerKey,
+//            placeType: PlaceType.Address,
+//            coordinate: coord,
+//            radius: 10
+//        )
+        
+        controller.didSelectGooglePlace { (place) -> Void in
+            print(place.description)
+            
+            //Dismiss Search
+            controller.isActive = false
+        }
+        
+        present(controller, animated: true, completion: nil)
+    }
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+
+target 'GooglePlacesSearchController_Example' do
+  pod "GooglePlacesSearchController", :path => "../"
+end
+
+target 'GooglePlacesSearchController_Tests' do
+  pod "GooglePlacesSearchController", :path => "../"
+
+  
+end
